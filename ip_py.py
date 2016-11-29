a92c9a0ba894e12f811ff7160ea54d46117a54a4
FujimoriHakuto <o109055h@mail.kyutech.jp>
2016-11-29 11:47:03 +0900

    meage?

--- ip_py.py
+++ ip_py.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-import socket
+import sys
+from PyQt4 import QtGui
 import netifaces
 
-print socket.gethostbyname(socket.gethostname())
-print netifaces.ifaddresses('wlan0')[netifaces.AF_INET][0]['addr']
+def main():
+  app = QtGui.QApplication(sys.argv)
+  w = QtGui.QWidget()
+  w.resize(300,50)
+  w.setWindowTitle("Get_IP_Address")
+  w.setWindowIcon(QtGui.QIcon("pythonlogo.png"))
+  print netifaces.ifaddresses('wlan0')[netifaces.AF_INET][0]['addr']
+  w.show()
+  sys.exit(app.exec_())
+  
+  
+  
+if __name__ == "__main__":
+  main()
