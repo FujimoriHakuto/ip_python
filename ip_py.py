d55ee645f21929bd6ef0ea836341a4d8423f932a
FujimoriHakuto <o109055h@mail.kyutech.jp>
2016-11-29 11:57:48 +0900

    complete

--- ip_py.py
+++ ip_py.py
@@ -6,12 +6,16 @@ from PyQt4 import QtGui
 import netifaces
 
 def main():
+  ip = netifaces.ifaddresses('wlan0')[netifaces.AF_INET][0]['addr']
   app = QtGui.QApplication(sys.argv)
   w = QtGui.QWidget()
   w.resize(300,50)
   w.setWindowTitle("Get_IP_Address")
   w.setWindowIcon(QtGui.QIcon("pythonlogo.png"))
-  print netifaces.ifaddresses('wlan0')[netifaces.AF_INET][0]['addr']
+  label = QtGui.QLabel(ip,w)
+  font=QtGui.QFont()
+  font.setPointSize(32)
+  label.setFont(font)
   w.show()
   sys.exit(app.exec_())
   
